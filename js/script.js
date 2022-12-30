eb88a66c8d53dc74c8fe10b6b0c515d5a0ffa731
Kateryna Tkachenko <es.tka4enko@gmail.com>
2022-12-30 23:42:12 +0200

    Added block playerMove

--- js/script.js
+++ js/script.js
@@ -7,7 +7,21 @@ let computerMove = 'nieznany ruch';
 if (randomNumber == 1) {
     computerMove = 'kamień';
 } else if (randomNumber == 2) {
-    computerMove = 'papier'
-} else computerMove = 'nożyce'
+    computerMove = 'papier';
+} else computerMove = 'nożyce';
 
 printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if (playerInput == '1') {
+    playerMove = 'kamień';
+} else if (randomNumber == 2) {
+    playerMove = 'papier';
+} else playerMove = 'nożyce';
+
+printMessage('Twój ruch to: ' + playerMove);
