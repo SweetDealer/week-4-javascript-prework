4ab8f945463c60d572e6a7308a41f6e7eaea5d6f
Kateryna Tkachenko <es.tka4enko@gmail.com>
2022-12-31 00:20:19 +0200

    Added game result

--- js/script.js
+++ js/script.js
@@ -20,8 +20,18 @@ let playerMove = 'nieznany ruch';
 
 if (playerInput == '1') {
     playerMove = 'kamień';
-} else if (randomNumber == 2) {
+} else if (playerInput == '2') {
     playerMove = 'papier';
-} else playerMove = 'nożyce';
+} else if (playerInput == '3') {
+    playerMove = 'nożyce';
+} else playerMove = 'nieznany ruch'
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if (computerMove == 'kamień' && playerMove == 'papier' || computerMove == 'papier' && playerMove == 'nożyce' || computerMove == 'nożyce' && playerMove == 'kamień') {
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'kamień' && playerMove == 'kamień' || computerMove == 'nożyce' && playerMove == 'nożyce' || computerMove == 'papier' && playerMove == 'papier') {
+    printMessage('Remis!');
+} else if (playerMove == 'nieznany ruch') {
+    printMessage('Musisz wybrać numer 1, 2 lub 3');
+} else printMessage('Ty przegrywasz!');
