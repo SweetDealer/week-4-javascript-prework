2f89bf8d12b4e80866381d70387b581e027c7900
Kateryna Tkachenko <es.tka4enko@gmail.com>
2023-01-02 12:13:10 +0200

    corrected remarks

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
+const papier = 'papier'
+const kamien = 'kamień'
+const nozyce = 'nożyce'
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = 'nieznany ruch';
 
-if (randomNumber == 1) {
-    computerMove = 'kamień';
-} else if (randomNumber == 2) {
-    computerMove = 'papier';
-} else computerMove = 'nożyce';
+if (randomNumber === 1) {
+    computerMove = kamien;
+} else if (randomNumber === 2) {
+    computerMove = papier;
+} else computerMove = nozyce;
 
 printMessage('Mój ruch to: ' + computerMove);
 
@@ -18,20 +22,21 @@ console.log('Gracz wpisał: ' + playerInput);
 
 let playerMove = 'nieznany ruch';
 
-if (playerInput == '1') {
-    playerMove = 'kamień';
-} else if (playerInput == '2') {
-    playerMove = 'papier';
-} else if (playerInput == '3') {
-    playerMove = 'nożyce';
-} else playerMove = 'nieznany ruch'
+if (playerInput === '1') {
+    playerMove = kamien;
+} else if (playerInput === '2') {
+    playerMove = papier;
+} else if (playerInput === '3') {
+    playerMove = nozyce;
+};
 
 printMessage('Twój ruch to: ' + playerMove);
 
-if (computerMove == 'kamień' && playerMove == 'papier' || computerMove == 'papier' && playerMove == 'nożyce' || computerMove == 'nożyce' && playerMove == 'kamień') {
+if (computerMove === kamien && playerMove === papier || computerMove === papier && playerMove === nozyce || computerMove === nozyce && playerMove === kamien) {
     printMessage('Ty wygrywasz!');
-} else if (computerMove == 'kamień' && playerMove == 'kamień' || computerMove == 'nożyce' && playerMove == 'nożyce' || computerMove == 'papier' && playerMove == 'papier') {
+} else if (computerMove === playerMove) {
     printMessage('Remis!');
-} else if (playerMove == 'nieznany ruch') {
+} else if (playerMove === 'nieznany ruch') {
     printMessage('Musisz wybrać numer 1, 2 lub 3');
 } else printMessage('Ty przegrywasz!');
+
